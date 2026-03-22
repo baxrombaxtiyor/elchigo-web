cd1ec516eb3ae177d984e01d1285c5f68e8d27c2
baxrom3110 <baxrom.claude@gmail.com>
2026-03-22 09:45:18 +0500

    fix firebase

--- elchigo/firebase.py
+++ elchigo/firebase.py
@@ -1,22 +1,8 @@
 # elchigo/firebase.py
-import firebase_admin
-from firebase_admin import credentials, firestore, auth
-from django.conf import settings
-
-
-def _init():
-    try:
-        firebase_admin.get_app()
-    except ValueError:
-        cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS)
-        firebase_admin.initialize_app(cred)
-
+from firebase_admin import firestore, auth
 
 def get_db():
-    _init()
     return firestore.client()
 
-
 def get_auth():
-    _init()
     return auth
